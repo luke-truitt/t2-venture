dc2dbc3c9a1d56ba55c7f4cffb0c50b74e04c238
Connor Johnson <connorjohnson@MacBook-Pro.fios-router.home>
2021-07-10 13:28:33 -0400

    Static json, points on layer

--- t2v-site/src/Map.js
+++ t2v-site/src/Map.js
@@ -1,49 +1,66 @@
 import React, { Component } from "react";
-import { Marker } from "react-map-gl";
-import ReactMapGL from "react-map-gl";
+import MapGL, { Source, Layer } from "react-map-gl";
+// import Geocoder from "react-mapbox-gl-geocoder";
 import "mapbox-gl/dist/mapbox-gl.css";
-import axios from "axios";
-import pin from "./mapbox-marker-icon-20px-gray.png";
+// import 'react-map-gl-geocoder/dist/mapbox-gl-geocoder.css'
+// import axios from "axios";
+// import grayMarker from "./grayMarker.png";
+import propertyMarkers from "./property_markers.json";
 
 const mapboxToken =
   "pk.eyJ1IjoiY21qNzAiLCJhIjoiY2txa2dhN3M0MDM4eDJucDJwZGZlaTF2dSJ9.hS4F34MrElRgSSmGJy_Oiw";
+
+const dataLayer = {
+  id: "markers",
+  type: "symbol",
+  // source: "points", // or maybe just data?
+  // "source-layer": "landuse",
+  // filter: ["==", "class", "park"],
+  layout: {
+    "icon-image": "marker-15", // likely wrong
+    "icon-allow-overlap": false,
+  },
+};
+
+// const params = {
+//   city: "austin",
+// };
 
 class Map extends Component {
   constructor() {
     super();
 
     this.state = {
-      markers: [],
       viewport: {
         width: "80%",
-        height: "100vh",
+        height: "80vh",
         latitude: 30.2672,
         longitude: -97.7431,
         zoom: 11,
       },
       loading: false,
       showSide: false,
-      clickObj: null
+      clickObj: null,
     };
     this.handleViewportChange = this.handleViewportChange.bind(this);
   }
 
   triggerSidebar(marker) {
-      console.log("test")
-    this.setState({clickObj: marker, showSide: true})
+    console.log("test");
+    this.setState({ clickObj: marker, showSide: true });
   }
 
-  componentDidMount() {
-    // have this align with the mapping instead of the api call maybe
-    this.setState({ loading: true }, () => {
-      axios.get("http://127.0.0.1:5000/").then((result) =>
-        this.setState({
-          loading: false,
-          markers: result.data.result,
-        })
-      );
-    });
-  }
+  // componentDidMount() {
+  //   // have this align with the mapping instead of the api call maybe
+  //   this.setState({ loading: true }, () => {
+  //     axios.get("http://127.0.0.1:5000/").then((result) =>
+  //       this.setState({
+  //         loading: false,
+  //         markers: result.data.result,
+  //       })
+  //     );
+  //   });
+  // }
 
   handleViewportChange(viewport) {
     this.setState((prevState) => ({
@@ -53,42 +70,61 @@ class Map extends Component {
 
   render() {
     return (
-        // make the mapping process asynchronous so some points show up? if thats what works?
-      <ReactMapGL
+      // make the mapping process asynchronous so some points show up? if thats what works?
+      <MapGL
         {...this.state.viewport}
-        onViewportChange={(viewport) => this.setState({ viewport })}
         // width="100%"
         // height="100%"
+        // latitude={30.2672}
+        // longitude={-97.7431}
+        // zoom={11}
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+        onViewportChange={(viewport) => this.setState({ viewport })}
         mapboxApiAccessToken={mapboxToken}
-        mapStyle="mapbox://styles/mapbox/streets-v10"
+        // interactiveLayerIds={['data']}
+        // onHover={onHover}
       >
-        {/* <Marker
-          // offsetTop={-48}
-          // offsetLeft={-24}
-          latitude={30.2672}
-          longitude={-97.743}
-          key={1}
-        >
-          <img src={pin} alt="Pin" />
-        </Marker> */}
-        {this.state.markers.slice(0,100).map(
-          (marker, index) => (
-            console.log(marker),
-            (
-              <Marker
-                // offsetTop={-48}
-                // offsetLeft={-24}
-                latitude={marker.lat}
-                longitude={marker.long}
-                key={index}
-                onClick={(marker) => this.triggerSidebar(marker)}
-              >
-          <img src={pin} alt="Pin" />
-              </Marker>
+        {/* <Geocoder
+          mapboxApiAccessToken={mapboxToken}
+          onSelected={(viewport) => this.setState({ viewport })}
+          viewport={this.state.viewport}
+          hideOnSelect={true}
+          value=""
+          queryParams={params}
+        /> */}
+        <Source type="geojson" data={propertyMarkers}>
+          <Layer {...dataLayer} />
+          {/* <Feature
+            coordinates={markerCoord}
+            onHover={this._onHover}
+            onEndHover={this._onEndHover}
+            onClick={this._onClickMarker}/> */}
+          {/* {this.state.markers.slice(0, 100).map(
+            (marker, index) => (
+              console.log(marker),
+              (
+                <Marker
+                  // offsetTop={-48}
+                  // offsetLeft={-24}
+                  latitude={marker.lat}
+                  longitude={marker.long}
+                  key={index}
+                  onClick={(marker) => this.triggerSidebar(marker)}
+                >
+                  <img src={grayMarker} alt="Pin" />
+                </Marker>
+              )
             )
-          )
-        )}
-      </ReactMapGL>
+          )} */}
+        </Source>
+        {/* {hoverInfo && (
+          <div className="tooltip" style={{left: hoverInfo.x, top: hoverInfo.y}}>
+            <div>State: {hoverInfo.feature.properties.name}</div>
+            <div>Median Household Income: {hoverInfo.feature.properties.value}</div>
+            <div>Percentile: {(hoverInfo.feature.properties.percentile / 8) * 100}</div>
+          </div>
+        )} */}
+      </MapGL>
     );
   }
 }
